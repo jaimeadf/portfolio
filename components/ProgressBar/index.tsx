07ec139de4aea441d0e4a8cb628cff7409bf765f
Jaime Filho <jaimeadanielfilho@hotmail.com>
2022-01-23 11:03:18 -0300

    refactor(components/ProgressBar): remove border radius prop

--- components/ProgressBar/index.tsx
+++ components/ProgressBar/index.tsx
@@ -7,19 +7,18 @@ export interface ProgressBarProps extends HTMLAttributes<HTMLDivElement> {
     value: number;
     color?: string;
     background?: string;
-    borderRadius?: string;
 }
 
 const ProgressBar: FunctionComponent<ProgressBarProps> = props => {
-    const { value, background, color, borderRadius, className, style } = props;
+    const { value, background, color, className, style } = props;
 
     return (
         <div
             {...props}
             className={classNames(styles.container, className)}
-            style={{ background, borderRadius, ...style }}
+            style={{ background, ...style }}
         >
-            <div className={styles.progress} style={{ width: `${value}%`, background: color, borderRadius }} />
+            <div className={styles.progress} style={{ width: `${value}%`, background: color }} />
         </div>
     );
 }
